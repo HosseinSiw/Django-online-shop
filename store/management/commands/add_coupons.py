9cee132621a908bff1887ef16cd6613c556bdfa6
Hossein siadati <hosseinsiadati01@gmail.com>
2024-09-03 16:49:33 +0330

    coupon command debuged

--- store/management/commands/add_coupons.py
+++ store/management/commands/add_coupons.py
@@ -8,11 +8,10 @@ from ...models import Coupon
 class Command(BaseCommand):
     help = "This command creates some random coupons and then inject them into Database."
     sampler = Faker()
-    sampler.seed(42)
 
-    def handle(self):
+    def handle(self, *args, **kwargs):
         for _ in range(10):
-            code = self.sampler.country()
+            code = self.sampler.country()  # I use random country names, you can change it on your owns.
             percent = random.randint(10, 40)
             Coupon.objects.create(code=code, percent=percent)
 
