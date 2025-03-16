557d895cd7b7ea0d148a690dce5d0d99dd002767
Hossein siadati <hosseinsiadati01@gmail.com>
2025-03-16 17:08:13 +0330

    3 apps finished

---
+++ cart/tests/test_views.py
@@ -0,0 +1,18 @@
+from rest_framework.test import APIClient
+from django.test import TestCase
+from django.urls import reverse
+from users.models import CustomUser as User
+from ..models import Cart
+
+
+class TestCartAPIViews(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='email@email.com', password='123/test/456', username='eraser')
+        self.cart = Cart.objects.get_or_create(user=self.user)
+
+    def test_add_to_cart(self):
+        add_to_cart_url = reverse('cart:api-v2:add-to-cart')
+        print(reverse(add_to_cart_url))
+        self.client.force_authenticate(user=self.user)
+        self.assertEqual(2, 2)
